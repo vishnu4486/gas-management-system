c9dca3642b138d482cd0ea0ce2bb4e1759aa2a71
Vishnu4486 <biradarvishnu89@gmail.com>
2020-08-14 06:48:37 +0530

    First commit

---
+++ gms-server-side-java/src/main/java/com/gms/GmsServerSideJavaApplication.java
@@ -0,0 +1,16 @@
+package com.gms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmsServerSideJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmsServerSideJavaApplication.class, args);
+		System.out.println("Wel come to india");
+		
+		
+	}
+
+}
---
+++ gms-server-side-java/src/test/java/com/gms/GmsServerSideJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmsServerSideJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
